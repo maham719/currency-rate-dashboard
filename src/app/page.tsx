d99220bcab813fe0ed5644d6f2d89b0ad06fa857
Syeda Maham Amjad <163040767+maham719@users.noreply.github.com>
2024-11-13 18:45:03 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { useState, useEffect } from "react";
+
 type CryptoData = {
   rates: {
     [key: string]: number; 
   };
-}
+};
+
 export default function CoinMarket() {
   const API_KEY = "1f77a513c5b29cbad1011074d33d62ed";
-  const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<CryptoData | null>(null); 
 
   useEffect(() => {
     fetch(`http://api.coinlayer.com/live?access_key=${API_KEY}`)
@@ -18,41 +20,44 @@ export default function CoinMarket() {
       });
   }, []);
 
-
-  return (<>
-    <div className="table-container">
-    <h1>Cryptocurrency Rates</h1>
-    <table className="table">
-      <thead className="heading">
-        <tr>
-          <th>Currency</th>
-          <th>Rate (USD)</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data?.rates &&
-          Object.entries(data.rates).map(([coin, rate]) => (
-            <tr key={coin}>
-              <td><strong><b>
-                {coin}</b></strong></td>
-              <td>${rate as number}</td>
+  return (
+    <>
+      <div className="table-container">
+        <h1>Cryptocurrency Rates</h1>
+        <table className="table">
+          <thead className="heading">
+            <tr>
+              <th>Currency</th>
+              <th>Rate (USD)</th>
             </tr>
-          ))}
-      </tbody>
-    </table>
-  </div>
-  <ul className="background">
-   <li></li>
-   <li></li>
-   <li></li>
-   <li></li>
-   <li></li>
-   <li></li>
-   <li></li>
-   <li></li>
-   <li></li>
-   <li></li>
-</ul>
-  </>
-);
+          </thead>
+          <tbody>
+            {data?.rates &&
+              Object.entries(data.rates).map(([coin, rate]) => (
+                <tr key={coin}>
+                  <td>
+                    <strong>
+                      <b>{coin}</b>
+                    </strong>
+                  </td>
+                  <td>${rate as number}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+      <ul className="background">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>
+  );
 }
